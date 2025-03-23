f25f891aeb5abf8a80aa3c41ff7086f1259807c0
Raphael Nembhard <nembhardraphael199@gmail.com>
2025-03-22 23:44:41 -0400

    minting added

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -11,7 +11,7 @@ use polkadot_sdk::polkadot_sdk_frame as frame;
 // Re-export all pallet parts, this is needed to properly import the pallet into the runtime.
 pub use pallet::*;
 
-#[frame::pallet]
+#[frame::pallet(dev_mode)]
 pub mod pallet {
 	use super::*;
 
@@ -21,6 +21,29 @@ pub mod pallet {
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
 
+	pub type Balance = u128;
+
 	#[pallet::storage]
-	pub type Value<T> = StorageValue<Value = u32>;
+	pub type TotalIssuance<T: Config> = StorageValue<_, Balance>;
+
+	#[pallet::storage]
+	pub type Balances<T: Config> = StorageMap<_, _, T::AccountId, Balance>;
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		pub fn unsafe_mint(
+			origin: T::RuntimeOrigin,
+			who: T::AccountId,
+			amount: Balance,
+		) -> DispatchResult {
+			// Ensure the origin is a signed extrinsic
+			let _anyone = ensure_signed(origin)?;
+			// Update the balance of the destination account
+			Balances::<T>::mutate(who, |balance| *balance = Some(balance.unwrap_or(0) + amount));
+			// Update the total token supply
+			TotalIssuance::<T>::mutate(|total| *total = Some(total.unwrap_or(0) + amount));
+
+			Ok(())
+		}
+	}
 }
